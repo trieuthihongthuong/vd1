d4fe545db87d42c1eb902d8b42e5bf04486ee8e5
trieuthihongthuong <156600026+trieuthihongthuong@users.noreply.github.com>
2024-05-03 18:36:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bai1</title>
-</head>
-<body>
-    <h2>Website
-        
-    </h2>
-</body>
-</html>
+<?php
+require_once("index.html");
